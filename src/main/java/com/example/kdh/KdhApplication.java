b9f53a9d7ccbd7249f71c5ce5feba751538c1acb
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2023-02-09 17:40:59 +0900

    :white_check_mark: 테스트 코드 추가

--- src/main/java/com/example/kdh/KdhApplication.java
+++ src/main/java/com/example/kdh/KdhApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KdhApplication {
+	// 메인 클래스 : 프로젝트 최상단 위에 위치
 
-    public static void main(String[] args) {
-        SpringApplication.run(KdhApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(KdhApplication.class, args);
+	}
 
 }
---
+++ src/main/java/com/example/kdh/web/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.kdh.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+	@RestController
+	1. 컨트롤러를 JSON을 반환하는 컨트롤러로 생성
+	2. @ResponseBody를 각 메서드 마다 선언했던 것을 한번에 사용하게 해줌
+ */
+@RestController
+public class HelloController {
+
+	/*
+		@GetMapping
+		- http method인 get 요청을 받을 수 있는 API 생성
+	 */
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+
+}
